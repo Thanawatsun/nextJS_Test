fe4f5c4e05236ce20b5d7da1fee99d166b5cec6e
Thanawat sritho <88428206+Thanawatsun@users.noreply.github.com>
2024-05-03 16:19:52 +0700

    first commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <>
+      <div>
+        <h1>contact</h1>
+        <p>Name: Thanwat Srito</p>
+        <p>From: ลาดกระบัง</p>
+      </div>
+    </>
+  )
+}
